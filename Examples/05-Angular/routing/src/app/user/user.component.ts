2d858d7ff4ee83857d8dd1e6ab63f1ea17ee1a42
Levin Baenninger <levin.baenninger@buhlergroup.com>
2024-03-21 16:11:15 +0100

    add routing examples

---
+++ Examples/05-Angular/routing/src/app/user/user.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styles: ``,
+})
+export class UserComponent implements OnInit, OnDestroy {
+  id!: string;
+  private subscription!: Subscription;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // this.id = this.activatedRoute.snapshot.params['id'];
+    this.subscription = this.activatedRoute.params.subscribe((params) => {
+      this.id = params['id'];
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  onNavigate() {
+    this.router.navigate(['/'], {
+      queryParams: { token: 100 },
+      fragment: 'anchor',
+    });
+  }
+}
